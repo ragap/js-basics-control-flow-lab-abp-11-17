ff8cf4f30ca79cee794c40686d267028277e43bf
ragas <sudha.puttagunta@gmail.com>
2017-11-16 12:28:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function switchOnCharmFromTip(tip)
 {
   if (tip === 'generous') {return 'Thank you so much.'}
   else if (tip === 'not as generous') {return 'Thank you.'}
-  else { return 'Bye'}
+  else { return "Bye"}
 }
