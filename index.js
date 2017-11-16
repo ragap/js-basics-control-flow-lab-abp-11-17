dfb2073f6137a03c8249476c55df5ffbaddfaf51
ragas <sudha.puttagunta@gmail.com>
2017-11-16 11:58:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 describe scuberGreetingForFeet(distnace)
 {
   if (distnace < 400) {return 'This one is on me!'}
-  else if (distnace > 2000 && distnace < 2500) {'I will gladly take your thirty bucks.'}
-  else if (distnace > 2500) {'No can do.'}
+  else if (distnace > 2000 && distnace < 2500) {return 'I will gladly take your thirty bucks.'}
+  else if (distnace > 2500) {return 'No can do.'}
 }
