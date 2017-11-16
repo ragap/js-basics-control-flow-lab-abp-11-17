2ba166746f87a58317136a49d119c3261428edbb
ragas <sudha.puttagunta@gmail.com>
2017-11-16 12:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
-describe scuberGreetingForFeet(distnace)
+describe scuberGreetingForFeet(distance)
 {
-  if (distnace < 400) {return 'This one is on me!'}
+  if (distance < 400) {return 'This one is on me!'}
   else if (distnace > 2000 && distnace < 2500) {return 'I will gladly take your thirty bucks.'}
   else if (distnace > 2500) {return 'No can do.'}
 }
