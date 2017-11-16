07c720274fe2f4892701ec81e8b5978efb88842c
ragas <sudha.puttagunta@gmail.com>
2017-11-16 12:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ function scuberGreetingForFeet(distance)
   else if (distance >= 2000 && distance <= 2500 ){return 'I will gladly take your thirty bucks.'}
   if (distance >= 2500){return 'No can do.'}
 }
+
+function ternaryCheckCity()
