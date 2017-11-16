8e55924f51ccc7805f6eb7b4df9a46479f62d559
ragas <sudha.puttagunta@gmail.com>
2017-11-16 11:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ describe scuberGreetingForFeet(ride)
 {
   if (ride <= 400) {return 'This one is on me!'}
   else if (ride > 2000 && ride < 2500) {'I will gladly take your thirty bucks.'}
-  else if (ride > 2500)
+  else if (ride > 2500) {'No can do.'}
 }
 
 describe ternaryCheckCity()
