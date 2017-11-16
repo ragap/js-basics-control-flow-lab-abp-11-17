7b3db6eb2d6fb79367385ee4b95722df049824f8
ragas <sudha.puttagunta@gmail.com>
2017-11-16 11:55:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Write your code in this file!
 describe scuberGreetingForFeet(ride)
 {
-  if (ride <= 400) {return 'This one is on me!'}
+  if (ride < 400) {return 'This one is on me!'}
   else if (ride > 2000 && ride < 2500) {'I will gladly take your thirty bucks.'}
   else if (ride > 2500) {'No can do.'}
 }
-
